24e3c520cdc0a7aa687d25f318031cd44643de4f
Md-Mustafa-Ali <49342063+Md-Mustafa-Ali@users.noreply.github.com>
2019-04-10 22:55:26 +0530

    Update ca1.c

--- ca1.c
+++ ca1.c
@@ -10,7 +10,7 @@ int main()
   if(p!=0)
    { int c;
      char buff2[100];
-     n=read(a[0],buff2,100);                                  \\read the string which is to be processed 
+     n=read(a[0],buff2,100);                                  //read the string which is to be processed 
      for(int i=0;i<n;i++)
       { c=(int)buff2[i];
         if(c>=97 && c<=122)
@@ -22,16 +22,16 @@ int main()
             buff2[i]=(char)c;
           }
       }
-     write(b[1],buff2,n);                                    \\write the resultant string in pipe
+     write(b[1],buff2,n);                                    //write the resultant string in pipe
    }
   else
    { char buff1[100];
      printf("Enter a string to reverse the case : \n");
-     n=read(0,buff1,100);                                    \\input from user
-     write(a[1],buff1,n);                                    \\write on pipe
-     wait(NULL);                                             \\switch to the child process
+     n=read(0,buff1,100);                                    //input from user
+     write(a[1],buff1,n);                                    //write on pipe
+     wait(NULL);                                             //switch to the child process
      printf("String with reversed case : \n");
-     n=read(b[0],buff1,n);                                   \\read processed string from pipe
-     write(1,buff1,n);                                       \\print on terminal
+     n=read(b[0],buff1,n);                                   //read processed string from pipe
+     write(1,buff1,n);                                       //print on terminal
    }
 }
